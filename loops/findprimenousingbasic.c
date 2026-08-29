3272dc2b731fe963d0ac012a53c220d3c7c8a18c
sonu-527 <mrsonum527@gmail.com>
2026-08-29 20:13:44 +0530

    xyz

---
+++ loops/findprimenousingbasic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int n;
+    int isprime = 1;
+    printf("Enter a number:");
+    scanf("%d", &n);
+    for (int i=2;i<n;i++){
+        if (n % i==0){
+            isprime = 0;
+            break;
+        }
+    }
+    if (isprime)
+      printf("prime");
+    else 
+    printf("not a prime");
+    return 0;
+}             
