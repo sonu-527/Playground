711c232bbc5ef31c4d972254280fa96f6e7e918d
sonu-527 <mrsonum527@gmail.com>
2026-09-03 19:02:15 +0530

    h

---
+++ Maths/sumofnos.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+    int a,b,c;
+    printf("Enter 1st no: ");
+    scanf("%d", &a);
+    printf("ENter 2nd no:");
+    scanf("%d", &b);
+    c = a+b;
+    printf("The sum of %d and %d are %d", a,b,c);
+    return 0;
+}
